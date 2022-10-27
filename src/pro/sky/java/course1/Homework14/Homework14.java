7a048708b62c48aaa3f290e246ac30bb5f51ee02
iShantaram <1agapovav987@gmail.com>
2022-10-28 00:09:17 +0300

    refactoring structure

--- src/pro/sky/java/course1/Homework14/Homework14.java
+++ src/pro/sky/java/course1/Homework14/Homework14.java
@@ -1,4 +1,6 @@
-public class Main {
+package pro.sky.java.course1.Homework14;
+
+public class Homework14 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
